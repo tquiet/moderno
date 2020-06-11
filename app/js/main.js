414f4b3fa0219b01cacd15476b80b1b7c3bb915f
Taras Tykhyi <taras_tykhyi@trsmac.local>
2020-06-11 23:00:33 +0300

    clear project

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,3 @@
 $(function(){
-  var mixer = mixitup('.products__inner-box');
-
-  $(".rate-star").rateYo({
-    rating: 5,
-    starWidth: "12px",
-    readOnly: true,
-  });
   
-  $('.product-slider__inner').slick({
-    dots: true,
-    arrows: false,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-  });
-
 });
